54cc7c8b68fc5cc1318053c0c9efed1098b9592c
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-11-29 17:26:43 +0600

    show tracing id and transaction

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CheckCircle } from "lucide-react";
-import { Link } from "react-router";
+import { Link, useSearchParams } from "react-router";
+import useAxios from "./../../../hooks/useAxiosSecure";
 
 const PaymentSuccess = () => {
+  const [searchParams] = useSearchParams();
+  const [paymentInfo, setPaymentInfo] = useState({});
+  const sessionId = searchParams.get("session_id");
+  const axiosSecure = useAxios();
+  useEffect(() => {
+    axiosSecure
+      .patch(`/payment-success?session_id=${sessionId}`)
+      .then((res) => {
+        console.log(res.data);
+        setPaymentInfo({
+          transactionId: res.data.transactionId,
+          trackingId: res.data.trackingId,
+        });
+      });
+  }, [sessionId, axiosSecure]);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-green-50 px-4">
       <div className="bg-white p-8 rounded-2xl shadow-xl max-w-md text-center">
@@ -11,7 +28,10 @@ const PaymentSuccess = () => {
         <h1 className="text-3xl font-bold text-green-700 mb-2">
           Payment Successful!
         </h1>
-
+        <div className="space-y-2 ">
+          <p>TransactionId: {paymentInfo?.transactionId}</p>
+          <p>TrackingId: {paymentInfo?.trackingId}</p>
+        </div>
         <p className="text-gray-600 mb-6">
           Thank you! Your payment has been processed successfully. Your parcel
           request is now confirmed.
